62374cd7f218c9ce773abc4cd2f32375c439b906
tvinente <vinente.talyta@gmail.com>
2021-11-16 22:21:53 -0300

    Criação de Calculadora

---
+++ src/br/com/dio/calculadora/Calculadora.java
@@ -0,0 +1,46 @@
+package br.com.dio.calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+			
+		int a, b;
+		
+		System.out.println("Digite o primeiro valor: ");
+		a = scan.nextInt();
+		System.out.println("Digite o segundo valor: ");
+		b = scan.nextInt();
+		
+		int soma = soma(a,b);
+		int subtração = subtração(a,b);
+		int multiplicação = multiplicação(a,b);
+		int divisão = divisão(a,b);
+		
+		System.out.println("soma "+ soma);
+		System.out.println("sub " + subtração);
+		System.out.println("mult " + multiplicação);
+		System.out.println("div " + divisão);
+		
+	}
+	
+	public static int soma(int a, int b) {	
+		return a + b;
+	}
+
+	public static int subtração(int a, int b) {	
+		return a - b;
+		
+	}
+	public static int divisão(int a, int b) {	
+		return a / b;
+		
+	}
+	public static int multiplicação(int a, int b) {	
+		return a * b;
+	}	
+	
+	
+	}
